6be9d7d906e409d4b3c264e9f65892d543c5cc92
Prem <89295807+greenprem@users.noreply.github.com>
2024-07-24 16:15:59 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 // src/app/page.tsx (or equivalent entry point)
 import MainStatus from '@/components/MainStatus'; // Adjust the import path as necessary
-import Header from '@/components/Header'
 import NavBar from '@/components/NavBar'
 import './globals.css'
 
