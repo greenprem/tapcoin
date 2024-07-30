d9ff34f5a4801f8343c39905248e478338faf129
Prem <89295807+greenprem@users.noreply.github.com>
2024-07-30 12:19:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,14 @@
 import MainStatus from '@/components/MainStatus'; // Adjust the import path as necessary
 import NavBar from '@/components/NavBar'
 import './globals.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Refer from '@/components/Refer'
 
 const HomePage: React.FC = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<MainStatus />} />
-            <Route path="/ref" element={<Refer />} />
-            {/* <Route path="/task" element={<TaskComponent />} />
-            <Route path="/tap" element={<TapComponent />} />
-            <Route path="/boost" element={<BoostComponent />} />
-            <Route path="/stats" element={<StatsComponent />} /> */}
-          </Routes>
-          <NavBar />
-        </div>
-      </div>
-    </Router>
-  );
+    <div className="flex flex-col min-h-screen">
+  <MainStatus />
+  <NavBar/>
+  </div>
+  )
 };
 
 export default HomePage;
