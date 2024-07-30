602ac5e85b90ea0b46aaf50e86e6e01b02809ed0
Prem <89295807+greenprem@users.noreply.github.com>
2024-07-30 12:55:47 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,42 @@
-// src/app/page.tsx (or equivalent entry point)
+'use client';
+// src/app/page.tsx
+import React, { useState } from 'react';
 import MainStatus from '@/components/MainStatus'; // Adjust the import path as necessary
-import NavBar from '@/components/NavBar'
-import './globals.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Refer from '@/components/Refer'
+import NavBar from '@/components/NavBar';
+import './globals.css';
+// Import other components here
+import Refer from '@/components/Refer';
+import TaskComponent from '@/components/TaskComponent';
+import BoostComponent from '@/components/BoostComponent';
+import StatsComponent from '@/components/StatsComponent';
 
 const HomePage: React.FC = () => {
+  const [activeView, setActiveView] = useState<string>('home');
+
+  const renderComponent = () => {
+    switch (activeView) {
+      case 'ref':
+        return <Refer />;
+      case 'task':
+        return <TaskComponent />;
+      case 'home':
+        return <MainStatus />;
+      case 'boost':
+        return <BoostComponent />;
+      case 'stats':
+        return <StatsComponent />;
+      default:
+        return <MainStatus />;
+    }
+  };
+
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        
-        <div className="flex-grow">
-          <Routes>
-            <Route path="/" element={<MainStatus />} />
-            <Route path="/ref" element={<Refer />} />
-            {/* <Route path="/task" element={<TaskComponent />} />
-            <Route path="/tap" element={<TapComponent />} />
-            <Route path="/boost" element={<BoostComponent />} />
-            <Route path="/stats" element={<StatsComponent />} /> */}
-          </Routes>
-          <NavBar />
-        </div>
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        {renderComponent()}
+        <NavBar activeView={activeView} onNavClick={setActiveView} />
       </div>
-    </Router>
+    </div>
   );
 };
 
