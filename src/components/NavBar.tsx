77f044b3755fc64b48a113e0014f85d3bdce237e
Prem <89295807+greenprem@users.noreply.github.com>
2024-07-30 12:08:06 +0530

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,29 +1,45 @@
 import React from 'react';
-import { FaCat, FaTasks, FaFire, FaChartBar } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import { FaCat, FaTasks, FaFire, FaChartBar, FaHome } from 'react-icons/fa';
 
 const NavBar: React.FC = () => {
   return (
     <div id='navb' className="flex justify-between items-center w-11/12 bg-purple-700 p-4 fixed bottom-2 rounded-3xl mx-auto inset-x-0">
-      <button className="flex flex-col items-center text-white">
+      <NavLink 
+        to="/ref" 
+        className={({ isActive }) => `flex flex-col items-center ${isActive ? 'text-yellow-400' : 'text-white'}`}
+      >
         <FaCat className="text-3xl" />
         <span className="text-sm mt-1">Ref</span>
-      </button>
-      <button className="flex flex-col items-center text-white">
+      </NavLink>
+      <NavLink 
+        to="/task" 
+        className={({ isActive }) => `flex flex-col items-center ${isActive ? 'text-yellow-400' : 'text-white'}`}
+      >
         <FaTasks className="text-3xl" />
         <span className="text-sm mt-1">Task</span>
-      </button>
-      <button className="flex flex-col items-center text-yellow-400">
-        <FaTasks className="text-3xl" />
+      </NavLink>
+      <NavLink 
+        to="/" 
+        className={({ isActive }) => `flex flex-col items-center ${isActive ? 'text-yellow-400' : 'text-white'}`}
+      >
+        <FaHome className="text-3xl" />
         <span className="text-sm mt-1">Tap</span>
-      </button>
-      <button className="flex flex-col items-center text-white">
+      </NavLink>
+      <NavLink 
+        to="/boost" 
+        className={({ isActive }) => `flex flex-col items-center ${isActive ? 'text-yellow-400' : 'text-white'}`}
+      >
         <FaFire className="text-3xl" />
         <span className="text-sm mt-1">Boost</span>
-      </button>
-      <button className="flex flex-col items-center text-white">
+      </NavLink>
+      <NavLink 
+        to="/stats" 
+        className={({ isActive }) => `flex flex-col items-center ${isActive ? 'text-yellow-400' : 'text-white'}`}
+      >
         <FaChartBar className="text-3xl" />
         <span className="text-sm mt-1">Stats</span>
-      </button>
+      </NavLink>
     </div>
   );
 }
