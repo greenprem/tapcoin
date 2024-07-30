bde5ac2e01743da05367f108edf01f5a48ec093d
Prem <89295807+greenprem@users.noreply.github.com>
2024-07-30 12:59:53 +0530

    Create StatsComponent.tsx

---
+++ src/components/StatsComponent.tsx
@@ -0,0 +1,8 @@
+// src/components/StatsComponent.tsx
+import React from 'react';
+
+const StatsComponent: React.FC = () => {
+  return <div>Stats Component</div>;
+};
+
+export default StatsComponent;
