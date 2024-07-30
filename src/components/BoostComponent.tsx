0168dfd10fb9e334c8035ecb428118c4425fd7fa
Prem <89295807+greenprem@users.noreply.github.com>
2024-07-30 12:59:27 +0530

    Create BoostComponent.tsx

---
+++ src/components/BoostComponent.tsx
@@ -0,0 +1,8 @@
+// src/components/BoostComponent.tsx
+import React from 'react';
+
+const BoostComponent: React.FC = () => {
+  return <div>Boost Component</div>;
+};
+
+export default BoostComponent;
